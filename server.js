ddd12ed2269589732934994537abdfaf764efead
elvaraelvara <107627367+elvaraelvara@users.noreply.github.com>
2023-04-02 15:46:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,73 @@
-// Import package 
-const grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const sqlite3 = require('sqlite3').verbose();
 
-// Define Proto path 
-const PROTO_PATH = './mahasiswa.proto';
-
-const options = {
+const packageDefinition = protoLoader.loadSync('./protofile.proto', {
   keepCase: true,
   longs: String,
   enums: String,
   defaults: true,
-  oneofs: true,
-}
+  oneofs: true
+});
 
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 
-// Load Proto 
-const mahasiswaProto = grpc.loadPackageDefinition(packageDefinition);
-
+const crudProto = grpc.loadPackageDefinition(packageDefinition).crud;
 const server = new grpc.Server();
+const db = new sqlite3.Database('./data.db');
 
-// Dummy data 
-const mahasiswa = {
-    mahasiswa: [
-        {
-            id: "1",
-            nama: "Rudi",
-            nrp: "5119",
-            nilai: 59
-          },
-          {
-            id: "2",
-            nama: "Budi",
-            nrp: "5118",
-            nilai: 60
-          }
-    ]
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS umat_gereja (id INTEGER PRIMARY KEY,nama TEXT NOT NULL,alamat TEXT NOT NULL,romo INTEGER NOT NULL, no_telepon INTEGER NOT NULL)");
+
+  server.addService(crudProto.CrudService.service, {
+    createUmatGereja: createUmatGereja,
+    readUmatGereja: readUmatGereja,
+    updateUmatGereja: updateUmatGereja,
+    deleteUmatGereja: deleteUmatGereja
+  });
+
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+});
+
+function createUmatGereja(call, callback) {
+  const { nama, alamat, romo, no_telepon } = call.request;
+  db.run(`INSERT INTO umat_gereja (nama, alamat, romo, no_telepon) VALUES (?, ?, ?, ?)`, [nama, alamat, romo, no_telepon], function(err) {
+    if (err) {
+      return callback(err);
+    }
+    const id = this.lastID;
+    callback(null, { id, nama, alamat, romo, no_telepon });
+  });
 }
 
-server.addService(mahasiswaProto.MahasiswaService.service, {
-  getAll: (_, callback) => {
-    callback(null, mahasiswa);
-  },
-  addMahasiswa: (call, callback) => {
-    const _mahasiswa = {...call.request};
-    mahasiswa.mahasiswa.push(_mahasiswa);
-    callback(null, _mahasiswa);
-  }
-})
+function readUmatGereja(call, callback) {
+  const { id } = call.request;
+  db.get(`SELECT * FROM umat_gereja WHERE id = ?`, [id], (err, row) => {
+    if (err || !row) {
+      return callback(err || new Error('Data not found'));
+    }
+    const { id: umat_gereja_id, nama, alamat, romo, no_telepon } = row;
+    callback(null, { id: umat_gereja_id, nama, alamat, romo, no_telepon });
+  });
+}
 
-// Start server 
-server.bindAsync(
-  "127.0.0.1:50051",
-  grpc.ServerCredentials.createInsecure(),
-  (error, port) => {
-    console.log("Server running at http://127.0.0.1:50051");
-    server.start();
-  }
-)
+function updateUmatGereja(call, callback) {
+  const { id, nama, alamat, romo, no_telepon } = call.request;
+  db.run(`UPDATE umat_gereja SET nama = ?, alamat = ?, romo = ?, no_telepon = ? WHERE id = ?`, [nama, alamat, romo, no_telepon, id], (err) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, { id, nama, alamat, romo, no_telepon });
+  });
+}
+
+function deleteUmatGereja(call, callback) {
+  const { id } = call.request;
+  db.run(`DELETE FROM umat_gereja WHERE id = ?`, [id], (err) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, { id });
+  });
+}
